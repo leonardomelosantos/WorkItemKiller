b1394cfef43c3792e8875c86531b948984642498
leonardomelosantos <leonardomelosantos@gmail.com>
2013-04-09 13:55:22 -0300

    Implementing search of work items and call witadmin.exe

--- WorkItemKiller/Form1.cs
+++ WorkItemKiller/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Windows.Forms;
 using Microsoft.TeamFoundation.Client;
 using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using System.Diagnostics;
+using System.Threading;
 
 namespace WorkItemKiller
 {
@@ -20,49 +22,78 @@ namespace WorkItemKiller
 
         private void button1_Click(object sender, EventArgs e)
         {
+            ProcessWorkItemsDeletion(@"http://srvtfs:8080/tfs/TFS_DOTNET", "cieepe", "lmsantos", "34696586", @"C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\witadmin.exe");
+        }
 
-            //TfsTeamProjectCollection collection = new TfsTeamProjectCollection(
-            //    new Uri(@"http://srvtfs:8080/tfs/TFS_DOTNET"),
-            //    new System.Net.NetworkCredential("CIEEPE\\lmsantos", "34696586"));
-
-            TfsTeamProjectCollection collection = new TfsTeamProjectCollection(
-                new Uri(@"http://srvtfs:8080/tfs/TFS_DOTNET"),
-                new System.Net.NetworkCredential("lmsantos", "34696586", "cieepe"));
-
-            collection.EnsureAuthenticated();
-
-            WorkItemStore wis = (WorkItemStore)collection.GetService(typeof(WorkItemStore));
-
-            //Iterate Through Projects
-            foreach (Project tfs_project in wis.Projects)
+        private void ProcessWorkItemsDeletion(string urlTfsCollection, string domain, string username, 
+            string password, string witadminPath)
+        {
+            try
             {
-                
-                //Console.WriteLine(tfs_project.Name);
-
-                //Perform WIQL Query 
-                WorkItemCollection wic = wis.Query(
-                   " SELECT [System.Id], [System.WorkItemType]," +
-                   " [System.State], [System.AssignedTo], [System.Title] " +
-                   " FROM WorkItems " +
-                   " WHERE [System.TeamProject] = '" + tfs_project.Name +
-                   "' ORDER BY [System.WorkItemType], [System.Id]");
-                foreach (WorkItem wi in wic)
+                TfsTeamProjectCollection collection = GetCollection(urlTfsCollection, domain, username, password);
+                if (collection != null)
                 {
-                    Console.WriteLine(wi.Title + "[" + wi.Type.Name + "]" + wi.Description);
+                    WorkItemStore wis = (WorkItemStore)collection.GetService(typeof(WorkItemStore));
+                    foreach (Project tfs_project in wis.Projects)
+                    {
+                        WorkItemCollection wic = wis.Query(
+                           " SELECT [System.Id], [System.WorkItemType]," +
+                           " [System.State], [System.AssignedTo], [System.Title] " +
+                           " FROM WorkItems " +
+                           " WHERE [System.TeamProject] = '" + tfs_project.Name +
+                           "' ORDER BY [System.WorkItemType], [System.Id]");
+                        foreach (WorkItem wi in wic)
+                        {
+                            if (wi.Title.StartsWith("[EXCLUIR]"))
+                            {
+                                CallLineCommandDeletion(witadminPath, urlTfsCollection, wi.Id);
+                                Thread.Sleep(2000);
+                            }
+                        }
+                    }
+                }
+            } catch (Exception ex) {
+                MessageBox.Show("Exception: " + ex.Message, "Exception");
+            }
+        }
+
+        private static TfsTeamProjectCollection GetCollection(string urlTfsCollection, string domain, string username, string password)
+        {
+            TfsTeamProjectCollection collection = null;
+            try
+            {
+                collection = new TfsTeamProjectCollection(new Uri(urlTfsCollection), new System.Net.NetworkCredential(username, password, domain));
+                if (collection != null)
+                {
+                    collection.EnsureAuthenticated();
                 }
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message, "Exception");
+            }
+            return collection;
+        }
 
-            /*
-            2. Open a Command Prompt window and then access the "IDE" directory for your installation of Visual Studio 2010/Visual Studio 2010 shell which should typically be "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE" or "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE"
-
-3. Run the following command (it may wrap on screen here, but the command should all be on one line):
-witadmin.exe destroywi /collection:https://tfsXX.discountasp.net/tfs/CollectionName /id:WorkItemID
-
-Please note that tfsXX should be replaced by your Team Foundation 2010 server, CollectionName will be the name of your collection and WorkItemID is the ID number for the Work Item. For example, if your Team Foundation Server is tfs21, your collection name is "mycollection" and your Work Item ID is 100, the command that you would run is:
-
-witadmin.exe destroywi /Collection:https://tfs21.discountasp.net/tfs/mycollection /id:100
-4. When you're prompted for the credentials, supply an administrator user name and the corresponding password.
-             */
+        private void CallLineCommandDeletion(string witadminPath, string urlTfsCollection, int workItemID)
+        {
+            try
+            {
+                ProcessStartInfo processStartInfo = new ProcessStartInfo(witadminPath);
+                processStartInfo.UseShellExecute = false;
+                processStartInfo.RedirectStandardError = true;
+                processStartInfo.RedirectStandardInput = true;
+                processStartInfo.RedirectStandardOutput = true;
+                processStartInfo.CreateNoWindow = false;
+                processStartInfo.ErrorDialog = false;
+                processStartInfo.WindowStyle = ProcessWindowStyle.Normal;
+                processStartInfo.Arguments = @" destroywi /collection:" + urlTfsCollection + " /id:" + workItemID + " /noprompt";
+                Process process = Process.Start(processStartInfo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message, "Exception");
+            }
         }
     }
 }
